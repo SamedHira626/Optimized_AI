fdcb25266b506a40053a23255c8b797560f91375
Samed Hıra <samedhira626@gmail.com>
2020-08-21 16:53:35 +0300

    Update program.py

--- program.py
+++ program.py
@@ -26,6 +26,7 @@ X = df[features]
 y = df['label']
 window = tk.Tk()
 window.geometry("1500x800") 
+window['background']='#e6c1c1'
 window.title("Samed Hıra")
 def plot_cm(cm,
                normalize=False,
@@ -89,11 +90,19 @@ def get_predictor():
     print("predictors = ",predictors)
     Xx = df[predictors]
     print("new df = ",Xx)
-    
+    global input_num
+    input_num=len(Xx.columns)
     return Xx
-# def get_data(Xxx):
-#       return Xxx
-def buttonFunction2():
+
+      
+        
+              
+              
+                    
+                    
+      
+
+def save_button():
       predictor = get_predictor()
       predictor = np.asarray(predictor)
       target = get_target()
@@ -101,8 +110,9 @@ def buttonFunction2():
  
 
 
-def buttonFunction():
-    Xx = get_predictor()     
+def run_button():
+    Xx = get_predictor()  
+    
     combo_value = problem.get()
     cross_random_value = int(cross_random.get()) 
     
@@ -114,30 +124,45 @@ def buttonFunction():
               Cv=int(cross.get())
               kernel = svm_kernel.get()
               if kernel == "linear":
-                    disable_linear()
+                    # svm_coef0.configure(state="disabled")  
+                    # svm_d.configure(state="disabled")
+                    # svm_g.configure(state="disabled")
+                    # svm_c.configure(state="normal")
                     C = float(svm_c.get())  
                     svm_model_linear = SVC(kernel = kernel, C = C )        
               elif kernel == "sigmoid":
+                    # svm_d.configure(state="disabled")
+                    # svm_c.configure(state="normal")
+                    # svm_coef0.configure(state="normal")
+                    # svm_g.configure(state="normal")
                     C = float(svm_c.get())
                     Coef0 = float(svm_coef0.get())
                     Gamma = float(svm_g.get())
                     svm_model_linear = SVC(kernel = kernel,C=C,gamma=Gamma, coef0=Coef0 )
               elif kernel == "rbf":
+                    # svm_coef0.configure(state="disabled")
+                    # svm_d.configure(state="disabled")
+                    # svm_c.configure(state="normal")
+                    # svm_g.configure(state="normal")
                     C = float(svm_c.get())
                     Gamma = float(svm_g.get())
                     svm_model_linear = SVC(kernel = kernel, C=C, gamma=Gamma)             
               elif kernel == "poly":
+                    # svm_c.configure(state="normal")
+                    # svm_coef0.configure(state="normal")
+                    # svm_g.configure(state="normal")
+                    # svm_d.configure(state="normal")
                     C = float(svm_c.get())
                     Coef0 = float(svm_coef0.get())
                     Gamma = float(svm_g.get())
                     Degree = int(svm_d.get()) 
                     svm_model_linear = SVC(kernel = kernel, C=C, gamma=Gamma, coef0=Coef0, degree=Degree) 
                     print("SVM çalışıyor")
-              cvs = cross_val_score(svm_model_linear, X , y ,cv = Cv)
+              cvs = cross_val_score(svm_model_linear, Xx , y ,cv = Cv)
               print(cvs)
-              svm_model_linear.fit(X,y)
-              svm_predictions = svm_model_linear.predict(X) 
-              accuracy = svm_model_linear.score(X, y) 
+              svm_model_linear.fit(Xx,y)
+              svm_predictions = svm_model_linear.predict(Xx) 
+              accuracy = svm_model_linear.score(Xx, y) 
               print("accuracy= ",accuracy)
               print ('c.r = ',classification_report(y, svm_predictions))
               # cm = confusion_matrix(y, svm_predictions) 
@@ -148,14 +173,14 @@ def buttonFunction():
                   ("Normalized confusion matrix", 'true')]
               for title, normalize in titles_options:
                     
-                          disp = plot_confusion_matrix(svm_model_linear, X, y,
+                          disp = plot_confusion_matrix(svm_model_linear, Xx, y,
                                  cmap=plt.cm.Blues,
                                  normalize=normalize)
                           disp.ax_.set_title(title)
 
                           print(title)
     # print(disp.confusion_matrix)
-
+              print ('Accuracy Score :',accuracy_score(y, svm_predictions))
               plt.show()
               report=classification_report(y, svm_predictions)
         
@@ -224,15 +249,17 @@ def buttonFunction():
               label_report = tk.Label(window, text = report)
               label_report.place(x = 1060 ,y = 50)
     elif combo_value == "MLP":
+          
         layers=int(mlp_layers.get())    
         epoch_num=int(itera.get())
         optm=float(opt.get())
-        Cv=int(cross.get())
+        
+        
         if layers == 1:
               first_number = int(first_layer.get())
               act_func1=activation_func1.get()
               model = keras.Sequential([
-                    keras.layers.Dense(first_number,input_dim=21, activation=act_func1),
+                    keras.layers.Dense(first_number,input_dim=input_num, activation=act_func1),
                     keras.layers.Dense(24,activation='softmax')
                     ]) 
       
@@ -242,7 +269,7 @@ def buttonFunction():
               act_func1=activation_func1.get()
               act_func2=activation_func2.get()
               model = keras.Sequential([
-                    keras.layers.Dense(first_number,input_dim=21, activation=act_func1),
+                    keras.layers.Dense(first_number,input_dim=input_num, activation=act_func1),
                     keras.layers.Dense(second_number, activation=act_func2),                    
                     keras.layers.Dense(24,activation='softmax')
                     ]) 
@@ -254,7 +281,7 @@ def buttonFunction():
               act_func2=activation_func2.get()
               act_func3=activation_func3.get()
               model = keras.Sequential([
-                    keras.layers.Dense(first_number,input_dim=21, activation=act_func1),
+                    keras.layers.Dense(first_number,input_dim=input_num, activation=act_func1),
                     keras.layers.Dense(second_number, activation=act_func2),
                     keras.layers.Dense(third_number, activation=act_func3),
                     keras.layers.Dense(24,activation='softmax')
@@ -266,51 +293,43 @@ def buttonFunction():
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
         
-        MLP = model.fit(X_train, y_train,epochs=epoch_num,validation_split=Cv)
+        if cross_random_value == 1:
+              Cv=int(cross.get())
+        
+              MLP = model.fit(Xx, y,epochs=epoch_num,validation_split=Cv)
 
         # MLP = model.fit(X_train, y_train,epochs=epoch_num)
         # scores = cross_val_score(MLP, X, y, cv=Cv)
         # print("Scores = ",scores)
-        y_pred = model.predict(X_test)
+              y_pred = model.predict(Xx)
         # print ('Accuracy Score :',accuracy_score(y_test, y_pred))
 # MLP = model.fit(X_train, Y_train)
-        y_pred=np.argmax(y_pred,axis=1)
+              y_pred=np.argmax(y_pred,axis=1)
 
-        from sklearn.metrics import confusion_matrix
-        cm = confusion_matrix(y_pred, y_test)
+              from sklearn.metrics import confusion_matrix
+              cm = confusion_matrix(y, y_pred)
+              report1=classification_report(y, y_pred)
+              print ('Accuracy Score :',accuracy_score(y, y_pred))
+        elif cross_random_value == 2:
+              random_percent=float(random.get())
+              X_train, X_test, y_train, y_test = train_test_split(Xx, y, test_size=random_percent, random_state=1)
+              MLP = model.fit(X_train, y_train,epochs=epoch_num)
 
+        
+              y_pred = model.predict(X_test)
+        
+              y_pred=np.argmax(y_pred,axis=1)
 
-# plt.imshow(cm,interpolation='nearest',cmap=plt.cm.Blues)
-# plt.show()
-
+              from sklearn.metrics import confusion_matrix
+              cm = confusion_matrix(y_pred, y_test)    
+              report1=classification_report(y_test, y_pred)
+              print ('Accuracy Score :',accuracy_score(y_test, y_pred))
         plot_cm(cm=cm, title='Confusion Matrix')
         
-        # MLP = model.fit(X_train, y_train)
-
-        # y_pred = model.predict(X_test)
-
-        # from sklearn.metrics import confusion_matrix
-        # # cm1 = confusion_matrix(y_pred, y_test)
-
         
-        # np.set_printoptions(precision=2)
-
-
-        # titles_options = [("Confusion matrix, without normalization", None),
-        #           ("Normalized confusion matrix", 'true')]
-        # for title, normalize in titles_options:
-        #     disp = plot_confusion_matrix(MLP, X_test, y_test,
-        #                          cmap=plt.cm.Blues,
-        #                          normalize=normalize)
-        #     disp.ax_.set_title(title)
-
-        #     print(title)
-
-        # plt.show()
-        # print (classification_report(y_test, y_pred))
         np.set_printoptions(precision=2)
-        #print (classification_report(y_test, y_pred)) 
-        report1=classification_report(y_test, y_pred)
+        
+        
         
         label_report = tk.Label(window, text = report1)
         label_report.place(x = 1060 ,y = 50)
@@ -318,8 +337,41 @@ def buttonFunction():
     
  
     print(problem.get())
+    
+    
+def disabling():
+      
+    
+    kernel = svm_kernel.get()
+    if kernel == "linear":
+            svm_coef0.configure(state="disabled")  
+            svm_d.configure(state="disabled")
+            svm_g.configure(state="disabled")
+            svm_c.configure(state="normal")
+                            
+    elif kernel == "sigmoid":
+            svm_d.configure(state="disabled")
+            svm_c.configure(state="normal")
+            svm_coef0.configure(state="normal")
+            svm_g.configure(state="normal")
+                   
+    elif kernel == "rbf":
+            svm_coef0.configure(state="disabled")
+            svm_d.configure(state="disabled")
+            svm_c.configure(state="normal")
+            svm_g.configure(state="normal")
+              
+    elif kernel == "poly":
+            svm_c.configure(state="normal")
+            svm_coef0.configure(state="normal")
+            svm_g.configure(state="normal")
+            svm_d.configure(state="normal")    
+              
+      
 
-label_output = tk.Label(window, text = "Outputs:")
+    
+
+label_output = tk.Label(window,bg="#E6C1C1", text = "Outputs:")
 label_output.place(x = 1180, y = 15)
         
 frame_left = tk.Frame(window, width = 400, height = 625, bg="white")
@@ -331,7 +383,7 @@ comboBox.place(x=15,y=15)
 
 ##########▓
 
-tk.Button(window, text='Browse Train Set', command=import_data).place(x=215,y=50)
+tk.Button(window, text='Browse DataSet', command=import_data).place(x=215,y=50)
 tk.Button(window, text='Read CSV', command=lambda: read_csv_file()).place(x=365,y=50)
 
 list_all = Listbox(window)
@@ -349,31 +401,31 @@ tk.Button(window, text="Delete Target", command=lambda: DeleteButton(list_target
 
 button = tk.Button(window, text = "Save", activebackground = "blue",
                         activeforeground = "black",
-                      command = buttonFunction2).place(x= 700,y=700)
+                      command = save_button).place(x= 700,y=700)
 
 #############        
 button = tk.Button(window, text = "Run", activebackground = "blue",
                         activeforeground = "black",
-                      command = buttonFunction)
+                      command = run_button)
 
 button.place(x = 900, y = 750)
 
 
-label_func = tk.Label(window, text = "Kernel Func:")
+label_func = tk.Label(window,bg="#E6C1C1", text = "Kernel Func:")
 label_func.place(x = 515, y = 45)
 svm_kernel = tk.StringVar()
-tk.Radiobutton(window, text = "Linear", value = "linear",variable = svm_kernel).place(x=515, y= 70)
-tk.Radiobutton(window, text = "RBF", value = "rbf", variable = svm_kernel).place(x= 515, y= 95)
-tk.Radiobutton(window, text = "Polynomial", value = "poly",variable = svm_kernel).place(x= 515, y= 120)
-tk.Radiobutton(window, text = "Sigmoid", value = "sigmoid", variable = svm_kernel).place(x= 515, y= 145)
+tk.Radiobutton(window, bg="#E6C1C1",text = "Linear", value = "linear",variable = svm_kernel).place(x=515, y= 70)
+tk.Radiobutton(window, bg="#E6C1C1",text = "RBF", value = "rbf", variable = svm_kernel).place(x= 515, y= 95)
+tk.Radiobutton(window, bg="#E6C1C1",text = "Polynomial", value = "poly",variable = svm_kernel).place(x= 515, y= 120)
+tk.Radiobutton(window, bg="#E6C1C1",text = "Sigmoid", value = "sigmoid", variable = svm_kernel).place(x= 515, y= 145)
 
 
-label_prm = tk.Label(window, text = "Model Parameters")
+label_prm = tk.Label(window,bg="#E6C1C1", text = "Model Parameters")
 label_prm.place(x = 515, y = 195)
 
 cross_random = tk.StringVar()
-tk.Radiobutton(window, text = "Cross Validation:", value = 1,variable = cross_random).place(x=15, y= 320)
-tk.Radiobutton(window, text = "Random Percent:", value = 2, variable = cross_random).place(x= 15, y= 370)
+tk.Radiobutton(window,bg="#E6C1C1", text = "Cross Validation:", value = 1,variable = cross_random).place(x=15, y= 320)
+tk.Radiobutton(window, bg="#E6C1C1",text = "Random Percent:", value = 2, variable = cross_random).place(x= 15, y= 370)
 
 
 # label_cross= tk.Label(window, text = "Cross Validation:")
@@ -388,64 +440,66 @@ random = tk.Entry(window, width = 12)
 random.insert(string = "",index = 0)
 random.place(x = 15,y = 395)
 
-label_c = tk.Label(window, text = "C:")
+label_c = tk.Label(window,bg="#E6C1C1", text = "C:")
 label_c.place(x = 515, y = 220)
 
 svm_c = tk.Entry(window, width = 10)
 svm_c.insert(string = "",index = 0)
 svm_c.place(x = 515,y = 245)
 
-label_coef0 = tk.Label(window, text = "Coef0:")
+label_coef0 = tk.Label(window,bg="#E6C1C1", text = "Coef0:")
 label_coef0.place(x = 515, y = 270)
 
 svm_coef0 = tk.Entry(window, width = 10)
 svm_coef0.insert(string = "",index = 0)
 svm_coef0.place(x = 515,y = 295)
 
-label_g = tk.Label(window, text = "Gamma:")
+label_g = tk.Label(window,bg="#E6C1C1", text = "Gamma:")
 label_g.place(x = 515, y = 320)
 
 svm_g = tk.Entry(window, width = 10)
 svm_g.insert(string = "",index = 0)
 svm_g.place(x = 515,y = 345)
 
-label_d = tk.Label(window, text = "Degree:")
+label_d = tk.Label(window,bg="#E6C1C1", text = "Degree:")
 label_d.place(x = 515, y = 370)
 
 svm_d = tk.Entry(window, width = 10)
 svm_d.insert(string = "",index = 0)
 svm_d.place(x = 515,y = 395)
 
-label2 = tk.Label(window, text = "MLP")
+disabling()
+
+label2 = tk.Label(window,bg="#E6C1C1", text = "MLP")
 label2.place(x = 65, y = 445)
 
-label2 = tk.Label(window, text = "# of hidden layer:")
+label2 = tk.Label(window,bg="#E6C1C1", text = "# of hidden layer:")
 label2.place(x = 15, y = 470)
 
 mlp_layers = tk.StringVar()
-tk.Radiobutton(window, text = "1", value = 1,variable = mlp_layers).place(x=15, y= 495)
-tk.Radiobutton(window, text = "2", value = 2, variable = mlp_layers).place(x= 65, y= 495)
-tk.Radiobutton(window, text = "3", value = 3,variable = mlp_layers).place(x= 115, y= 495)
+tk.Radiobutton(window,bg="#E6C1C1", text = "1", value = 1,variable = mlp_layers).place(x=15, y= 495)
+tk.Radiobutton(window, bg="#E6C1C1",text = "2", value = 2, variable = mlp_layers).place(x= 65, y= 495)
+tk.Radiobutton(window, bg="#E6C1C1",text = "3", value = 3,variable = mlp_layers).place(x= 115, y= 495)
 
-label_first_layer = tk.Label(window, text = "Neurons in 1st layer:")
+label_first_layer = tk.Label(window,bg="#E6C1C1", text = "Neurons in 1st layer:")
 label_first_layer.place(x = 15, y = 545)
 first_layer = tk.Entry(window, width = 10)
 first_layer.insert(string = "",index = 0)
 first_layer.place(x = 160,y = 545)
 
-label_second_layer = tk.Label(window, text = "Neurons in 2nd layer:")
+label_second_layer = tk.Label(window, bg="#E6C1C1",text = "Neurons in 2nd layer:")
 label_second_layer.place(x = 15, y = 570)
 second_layer = tk.Entry(window, width = 10)
 second_layer.insert(string = "",index = 0)
 second_layer.place(x = 160,y = 570)
 
-label_third_layer = tk.Label(window, text = "Neurons in 3rd layer:")
+label_third_layer = tk.Label(window,bg="#E6C1C1", text = "Neurons in 3rd layer:")
 label_third_layer.place(x = 15, y = 595)
 third_layer = tk.Entry(window, width = 10)
 third_layer.insert(string = "",index = 0)
 third_layer.place(x = 160,y = 595)
 
-label_act = tk.Label(window, text = "Activation Funcctions:")
+label_act = tk.Label(window,bg="#E6C1C1", text = "Activation Functions:")
 label_act.place(x = 270, y = 515)
 
 activation_func1 = tk.StringVar()
@@ -460,22 +514,18 @@ activation_func3 = tk.StringVar()
 comboBox = ttk.Combobox(window, textvariable = activation_func3, values = ("relu","linear","sigmoid","softmax","tanh"), state= "readonly")
 comboBox.place(x=250,y=595)
 
-label_iter = tk.Label(window, text = "Epoch:")
+label_iter = tk.Label(window,bg="#E6C1C1", text = "Epoch:")
 label_iter.place(x = 15, y = 645)
 itera = tk.Entry(window, width = 10)
 itera.insert(string = "",index = 0)
 itera.place(x = 160,y = 645)
 
-label_opt = tk.Label(window, text = "Learning rate:")
+label_opt = tk.Label(window,bg="#E6C1C1", text = "Learning rate:")
 label_opt.place(x = 15, y = 670)
 opt = tk.Entry(window, width = 10)
 opt.insert(string = "",index = 0)
 opt.place(x = 160,y = 670)
 
 
-
+disabling()
 window.mainloop()
-
-
-
-
